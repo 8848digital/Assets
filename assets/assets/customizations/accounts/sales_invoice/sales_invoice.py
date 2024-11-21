45d524e5f401f755adf99856baf88a32e874e72b
IamSaiyyamChhetri <IamSaiyyamChhetri>
2024-11-21 16:14:13 +0530

    feat: Asset Sold SI Name reference

--- assets/assets/customizations/accounts/sales_invoice/sales_invoice.py
+++ assets/assets/customizations/accounts/sales_invoice/sales_invoice.py
@@ -1,4 +1,5 @@
-from assets.assets.customizations.accounts.sales_invoice.doc_events import validate_fixed_asset, set_income_account_for_fixed_assets
+from assets.assets.customizations.accounts.sales_invoice.doc_events import (
+    check_n_update_asset_doc, set_income_account_for_fixed_assets, validate_fixed_asset)
 from assets.assets.customizations.accounts.sales_invoice.override import make_item_gl_entries
 from erpnext.accounts.doctype.sales_invoice.sales_invoice import SalesInvoice
 
@@ -9,3 +10,7 @@ class AssetsSalesInvoice(SalesInvoice):
 def validate(doc, method = None):
     validate_fixed_asset(doc)
     set_income_account_for_fixed_assets(doc)
+
+def on_submit(doc, method =None):
+    if method == "on_submit":
+        check_n_update_asset_doc(doc)
--- assets/hooks.py
+++ assets/hooks.py
@@ -187,6 +187,7 @@ doc_events = {
 	},
     "Sales Invoice": {
 		"validate": "assets.assets.customizations.accounts.sales_invoice.sales_invoice.validate",
+		"on_submit": "assets.assets.customizations.accounts.sales_invoice.sales_invoice.on_submit",
 	},
 	# "Landed Cost Voucher": {
 	# 	"validate": "assets.assets.customizations.stock.landed_cost_voucher.landed_cost_voucher.validate",
