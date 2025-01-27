5beb425f40fbaf575ca3f8949b2bb63e50080f49
prateek <prateek@8848digital.com>
2025-01-27 16:08:18 +0530

    test:test_is_existing_asset_and_asset_depreciation_schedule_TC_FA_021

--- assets/assets/doctype/asset/test_asset.py
+++ assets/assets/doctype/asset/test_asset.py
@@ -912,7 +912,10 @@ class TestAsset(AssetSetup):
 				"item_code": item_code,
 				"item_name": item_code,
 				"item_group":"Products",
+				"is_stock_item": 0,
 				"is_fixed_asset": 1 , # Marking as fixed asset
+				"gst_hsn_code":"01011010",
+				"asset_naming_series": "ACC-ASS-.YYYY.-",
 				"asset_category":"Test_Category"
 			}).insert()
 
