fda4b55106666a936a84e1dbf2d344d2db649117
prateek <prateek@8848digital.com>
2025-01-28 15:54:04 +0530

    test:test_asset_movement_receipt_location_change_TC_FA_031

--- assets/assets/doctype/asset_movement/test_asset_movement.py
+++ assets/assets/doctype/asset_movement/test_asset_movement.py
@@ -551,6 +551,82 @@ class TestAssetMovement(unittest.TestCase):
 			asset_movement.submit()
 			frappe.db.commit()
 
+	#TC_FA_031
+	def test_asset_movement_receipt_location_change_TC_FA_031(self):
+		company = "_Test Company"
+		item_code = "Test_item_receipt"
+		# Ensure prerequisites exist
+		if not frappe.db.exists("Company", company):
+			create_child_company()
+		if not frappe.db.exists("Item", item_code):
+			frappe.get_doc({
+				"doctype": "Item",
+				"item_code": item_code,
+				"item_name": item_code,
+				"gst_hsn_code":"01011010",
+				"is_stock_item": 0,
+				"is_fixed_asset": 1,
+				"auto_create_assets": 1,
+				"asset_category": "Test_Category"
+			}).insert()
+		
+		if not frappe.db.exists("Employee", "Test_employee_issue"):
+			employee_doc = frappe.get_doc({
+				"doctype": "Employee",
+				"employee_name": "Test_employee_issue",
+				"first_name": "Test_employee_issue",
+				"gender": "Male",
+				"date_of_birth": "1990-01-01",
+				"date_of_joining": "2023-01-01",
+				"status": "Active",
+				"company": company
+			}).insert()
+
+		target_asset = frappe.get_doc({
+			"doctype": "Asset",
+			"company": company,
+			"item_code": item_code,
+			"asset_name": item_code,
+			"asset_category":"Test_Category",
+			"location": "Test Location",
+			"is_existing_asset":1,
+			"custodian":employee_doc.name,
+			"owner":"Company",
+			"asset_owner":"Company",
+			"available_for_use_date":"02-04-2024",
+			"gross_purchase_amount":8000,
+			"total_asset":8000,
+			"asset_quantity":1,
+			"purchase_date":"01-04-2024",
+			"finance_books":[{
+				"finance_book":"2024-2025",
+				"frequency_of_depreciation":1,
+				"depreciation_method":"Straight Line",
+				"depreciation_start_date":"01-06-2025",
+				"total_number_of_depreciations":12,
+				"total_number_of_booked_depreciations":7,
+				"value_after_depreciation":5000
+					}]
+		}).insert()
+		target_asset.submit()
+		frappe.db.commit()
+
+		if target_asset:
+			asset_movement = frappe.get_doc({
+				"doctype":"Asset Movement",
+				"company":company,
+				"purpose":"Receipt",
+				"assets":[{
+					"asset":target_asset,
+					"source_location":"Test Location",
+					"from_employee":employee_doc.name,
+					"target_location":"Field 1",
+				}]
+			}) 
+			asset_movement.insert()
+			asset_movement.submit()
+			frappe.db.commit()
+
 	def setUp(self):
 		frappe.db.set_value(
 			"Company", "_Test Company", "capital_work_in_progress_account", "CWIP Account - _TC"
