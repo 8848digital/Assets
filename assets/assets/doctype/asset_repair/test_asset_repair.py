d7e68ed678ccea7f41733d80959e09204a96a482
prateek <prateek@8848digital.com>
2025-03-19 14:46:29 +0530

    Fix: changing finance book from static to dynamic

--- assets/assets/doctype/asset_repair/test_asset_repair.py
+++ assets/assets/doctype/asset_repair/test_asset_repair.py
@@ -9,7 +9,7 @@ from erpnext.stock.doctype.serial_and_batch_bundle.test_serial_and_batch_bundle 
 	get_serial_nos_from_bundle,
 	make_serial_batch_bundle,
 )
-from frappe.utils import flt, nowdate, nowtime, today ,add_days,now_datetime
+from frappe.utils import flt, nowdate, nowtime, today ,add_days,now_datetime,getdate
 from erpnext.setup.doctype.company.test_company import create_child_company
 from assets.assets.doctype.asset.asset import (
 	get_asset_account,
@@ -63,7 +63,6 @@ class TestAssetRepair(unittest.TestCase):
 			frappe.get_doc(item_data).insert()
 
 		today = nowdate()
-		
 		target_asset = frappe.get_doc({
 			"doctype": "Asset",
 			"company": company,
@@ -83,7 +82,7 @@ class TestAssetRepair(unittest.TestCase):
 			"is_fully_depreciated": 1,
 			"maintenance_required": 1,
 			"finance_books": [{
-				"finance_book": "2024-2025",  # Dynamic financial year
+				"finance_book": f"{getdate('2024-04-01').year}-{getdate('2025-03-31').year}",  # Dynamic financial year
 				"frequency_of_depreciation": 1,
 				"depreciation_method": "Straight Line",
 				"depreciation_start_date": add_days(today, 365),  # One year later
