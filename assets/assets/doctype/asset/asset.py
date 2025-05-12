e6f22120db6074de7d548250b4e0f49d5ecf56ae
vinod-8848digital <vinod@8848digital.com>
2025-05-12 14:06:47 +0530

    feat: asset commits are added from standard erpnext-version-15 till 6th may

--- assets/assets/doctype/asset/asset.py
+++ assets/assets/doctype/asset/asset.py
@@ -43,6 +43,8 @@ from assets.assets.doctype.asset_depreciation_schedule.asset_depreciation_schedu
 
 class Asset(AccountsController):
 	# begin: auto-generated types
+	# ruff: noqa
+
 	# This code is auto-generated. Do not modify anything in this block.
 
 	from typing import TYPE_CHECKING
@@ -120,6 +122,7 @@ class Asset(AccountsController):
 		total_asset_cost: DF.Currency
 		total_number_of_depreciations: DF.Int
 		value_after_depreciation: DF.Currency
+	# ruff: noqa
 	# end: auto-generated types
 
 	def validate(self):
@@ -1211,7 +1214,6 @@ def get_values_from_purchase_doc(purchase_doc_name, item_code, doctype):
 		frappe.throw(_(f"Selected {doctype} does not contain the Item Code {item_code}"))
 
 	first_item = matching_items[0]
-	is_multiple_items = len(matching_items) > 1
 
 	return {
 		"company": purchase_doc.company,
@@ -1220,7 +1222,6 @@ def get_values_from_purchase_doc(purchase_doc_name, item_code, doctype):
 		"asset_quantity": first_item.qty,
 		"cost_center": first_item.cost_center or purchase_doc.get("cost_center"),
 		"asset_location": first_item.get("asset_location"),
-		"is_multiple_items": is_multiple_items,
 		"purchase_receipt_item": first_item.name if doctype == "Purchase Receipt" else None,
 		"purchase_invoice_item": first_item.name if doctype == "Purchase Invoice" else None,
 	}
