34622997d684979f215fbc1180dcf771caca31d9
Navin-S-R <navin@aerele.in>
2026-01-26 21:17:06 +0530

    fix(asset capitalization): update asset values using db_set

--- assets/assets/doctype/asset_capitalization/asset_capitalization.py
+++ assets/assets/doctype/asset_capitalization/asset_capitalization.py
@@ -694,20 +694,13 @@ class AssetCapitalization(StockController):
 		if self.docstatus == 2:
 			gross_purchase_amount = asset_doc.gross_purchase_amount - total_target_asset_value
 			purchase_amount = asset_doc.purchase_amount - total_target_asset_value
-			total_asset_cost = asset_doc.total_asset_cost - total_target_asset_value
+			asset_doc.db_set("total_asset_cost", asset_doc.total_asset_cost - total_target_asset_value)
 		else:
 			gross_purchase_amount = asset_doc.gross_purchase_amount + total_target_asset_value
 			purchase_amount = asset_doc.purchase_amount + total_target_asset_value
-			total_asset_cost = asset_doc.total_asset_cost + total_target_asset_value
 
-		asset_doc.db_set(
-			{
-				"gross_purchase_amount": gross_purchase_amount,
-				"purchase_amount": purchase_amount,
-				"total_asset_cost": total_asset_cost,
-			}
-		)
-
+		asset_doc.db_set("gross_purchase_amount", gross_purchase_amount)
+		asset_doc.db_set("purchase_amount", purchase_amount)
 
 		frappe.msgprint(
 			_(
