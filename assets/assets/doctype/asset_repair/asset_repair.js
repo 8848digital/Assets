810ad683355d44585139529883020fa98fda5f3f
ljain112 <ljain112@gmail.com>
2026-03-09 18:03:59 +0530

    fix: correct logic for repair cost in asset repair

--- assets/assets/doctype/asset_repair/asset_repair.js
+++ assets/assets/doctype/asset_repair/asset_repair.js
@@ -120,7 +120,9 @@ frappe.ui.form.on("Asset Repair", {
 					purchase_invoice: frm.doc.purchase_invoice,
 				},
 				callback: function (r) {
-					frm.set_value("repair_cost", r.message || 0);
+					if (r.message) {
+						frm.set_value("repair_cost", r.message);
+					}
 				},
 			});
 		} else {
